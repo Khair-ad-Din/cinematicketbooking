02b4c10bb6674461b28b79b244750257ae762c23
Sergio Chudnovets <134851371+Khair-ad-Din@users.noreply.github.com>
2025-09-20 11:49:04 +0200

    add/ Swipe Functionality Mobile + Desktop

--- src/app/components/movie-swipe-card/movie-swipe-card.component.ts
+++ src/app/components/movie-swipe-card/movie-swipe-card.component.ts
@@ -20,7 +20,90 @@ export class MovieSwipeCardComponent {
 
   isFlipped = signal<boolean>(false);
 
+  private startX = 0;
+  private startY = 0;
+  private endX = 0;
+  private endY = 0;
+  private minSwipeDistance = 50; // Min pixels for a swipe.
+  private isMouseDown = false;
+
   flipCard() {
     this.isFlipped.update((current) => !current);
+  }
+
+  onTouchStart(event: TouchEvent) {
+    this.startX = event.touches[0].clientX;
+    this.startY = event.touches[0].clientY;
+  }
+
+  onTouchEnd(event: TouchEvent) {
+    this.endX = event.changedTouches[0].clientX;
+    this.endY = event.changedTouches[0].clientY;
+    this.handleSwipe();
+  }
+
+  onMouseDown(event: MouseEvent) {
+    this.startX = event.clientX;
+    this.startY = event.clientY;
+    this.isMouseDown = true;
+    event.preventDefault();
+  }
+
+  onMouseUp(event: MouseEvent) {
+    this.endX = event.clientX;
+    this.endY = event.clientY;
+    this.handleSwipe();
+    this.isMouseDown = false;
+  }
+
+  onMouseLeave(event: MouseEvent) {
+    if (this.isMouseDown) {
+      this.endX = event.clientX;
+      this.endY = event.clientY;
+      this.handleSwipe();
+      this.isMouseDown = false;
+    }
+  }
+
+  private handleSwipe() {
+    const deltaX = this.endX - this.startX;
+    const deltaY = this.endY - this.startY;
+
+    // Determines if horizontal or vertical swipe
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      // Horizontal swipe
+      if (Math.abs(deltaX) > this.minSwipeDistance) {
+        if (deltaX > 0) {
+          this.swipeRight(); // Not seen + Liked + Saved
+        } else {
+          this.swipeLeft(); // Not seen + Not Liked + Not Saved
+        }
+      }
+    } else {
+      // Vertical Swipe
+      if (Math.abs(deltaY) > this.minSwipeDistance) {
+        if (deltaY < 0) {
+          this.swipeUp(); // Seen + Liked + Save
+        } else {
+          this.swipeDown(); // Seen + Not Liked + Not Saved
+        }
+      }
+    }
+  }
+
+  swipeUp() {
+    console.log('Seen + Liked + Save');
+  }
+
+  swipeDown() {
+    console.log('Seen + Not Liked + Not Saved');
+  }
+
+  swipeLeft() {
+    console.log('Not seen + Not Liked + Not Saved');
+  }
+
+  swipeRight() {
+    console.log('Not seen + Liked + Saved');
   }
 }
